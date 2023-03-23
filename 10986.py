8933eb0a53a66d66221b5e360704842bd4b575c4
paradise <junmate12@gmail.com>
2023-03-23 14:34:21 +0900

    2023-03-23

--- 10986.py
+++ 10986.py
@@ -1,3 +1,45 @@
 # 나머지 합
-print(1)
-# 1
+# 수 n개 연속이 주어짐. 부분 구간 합이 m으로 나누어 떨어지는 구간의 개수는?
+# 5개, 3으로 나누어 떨어지는
+# 1 2 3 1 2
+# (1, 2), (1, 2, 3), (1,2,3,1,2), (2,3,1), (3), (3,1,2), (1,2) 총 7개
+import sys
+
+# 파이썬은 default 입력을 문자열로 받는다.
+n, m = map(int, sys.stdin.readline().split())
+# print(m,n)
+array = list(map(int, sys.stdin.readline().split()))
+# print(array)
+
+# sum1 은 부분합 리스트
+sum1 = [0]*(n+1)
+for i in range(1, len(array)+1):
+    sum1[i] = sum1[i-1] + array[i-1]
+# print(sum1)
+
+# prefix는 sum1을 m으로 나눈 나머지
+# 혹은 prefix[r] 을 나머지가 r인 부분합의 개수로 지정해준다.
+prefix = [0]*(m+1)
+for i in range(len(sum1)):
+    r = sum1[i]%m
+    prefix[r] += 1 # 추가됨
+# print(prefix)
+
+# # prefix에서 0,1,2, ... , n-1 을 찾은 후 나머지가 같은 배열값들끼리 조합 경우의 수를 계산한다.
+# # combination 계산 = i*(i+1)/2
+# result = []
+# for i in range(m):
+#     count = 0
+#     for j in range(n+1):
+#         if prefix[j] == i:
+#             count += 1
+#     result.append(count*(count-1)//2)
+# # print(result)
+# print(sum(result))
+
+result = []
+for i in range(len(prefix)):
+    if (prefix[i] != 0):
+        l = prefix[i]
+        result.append(l*(l-1)//2)
+print(sum(result))
---
+++ 11660.py
@@ -0,0 +1,33 @@
+# 구간합구하기 5
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = []
+for i in range(n):
+    a.append(list(map(int, sys.stdin.readline().split())))
+# a = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]]
+b = []
+for i in range(m):
+    b.append(list(map(int, sys.stdin.readline().split())))
+# b = [[2,2,3,4], [3,4,3,4], [1,1,4,4]]
+# print(b)
+
+prefix = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix[i][j] = (
+            a[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1]
+        )
+
+totalList = []
+for i in range(m):
+    x1, y1 = b[i][0], b[i][1]
+    x2, y2 = b[i][2], b[i][3]
+    total = (
+        prefix[x2][y2]
+        - prefix[x1 - 1][y2]
+        - prefix[x2][y1 - 1]
+        + prefix[x1 - 1][y1 - 1]
+    )
+    totalList.append(total)
+print(totalList)
