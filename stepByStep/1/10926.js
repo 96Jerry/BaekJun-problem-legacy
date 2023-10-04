516f91b32948c02adf28a4f9e6811b8a1b067303
W N <junmate12@naver.com>
2023-10-04 23:32:53 +0900

    style: 주석생성

--- stepByStep/1/10926.js
+++ stepByStep/1/10926.js
@@ -3,3 +3,5 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "../../input.txt"
 const input = fs.readFileSync(filePath).toString().trim()
 
 console.log(input + '??!')
+
+// github 설정 오류로 잔디가 심어지지 않는 문제 발생
